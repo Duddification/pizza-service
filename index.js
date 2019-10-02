209b13bd4b2f37dbf51af1e24bff7c9f092c1170
Duddification <nick_dodd@hotmail.co.uk>
2019-10-02 13:28:37 +0100

    added the get func

--- index.js
+++ index.js
@@ -15,14 +15,47 @@ const pizzas = [
         name: "meat feast",
         size: 24,
         spicy: false
+    },
+    {
+        id: 3,
+        name: "veggie",
+        size: 24,
+        spicy: false
+    },
+    {
+        id: 4,
+        name: "spicy veg",
+        size: 12,
+        spicy: true
+
     }
 
 ];
 
-app.get('/pizzas', (req,resp)=> {
+app.get('/pizzas', (req, resp) => {
     console.log('Returning pizzas');
-    resp.setHeader('Content-Type','application/json');
+    resp.setHeader('Content-Type', 'application/json');
     resp.send(pizzas);
+});
+
+app.get('./pizzas/*', (req, resp) => {
+
+    const id = parseInt(req.params[0]);
+
+    console.log('Endpoint /pizzas/${id} hit');
+
+    const pizza = pizzas.find(pizza => pizza.id === id);
+
+    
+
+    resp.setHeader('Content-Type', 'application/json');
+//    resp.send(pizzas);
+
+    if (pizza) {
+        resp.send(pizza);
+    } else {
+        resp.status(404).send(`{error:"Pizza with ID ${id} not found"}`);
+    }
 });
 
 console.log(`server listening on ${port}`);
